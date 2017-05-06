50cea2882044a9e4bf7909113f2e0b3441ec30f4
TheConnMan <bcconn2112@gmail.com>
2017-05-06 16:13:41 -0400

    Add view apology page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,25 @@
 angular.module('app', ['ngResource', 'ngRoute', 'angularMoment'])
 .controller('MainController', ['$scope', '$resource', '$route', '$routeParams', '$location', function($scope, $resource, $route, $routeParams, $location) {
 
-  var Apology = $resource('/apology/:apologyId', {
+  $scope.Apology = $resource('/apology/:apologyId', {
     apologyId: '@id'
   });
 
-  $scope.apologies = Apology.query({
+  $scope.apologies = $scope.Apology.query({
     limit: 20,
     sort: 'createdAt DESC'
+  });
+
+  $scope.viewApology = function(apology) {
+    $location.path('/apology/' + apology.user.name.toLowerCase().split(' ').join('-') + '/' + apology.id);
+  };
+}])
+
+.controller('ApologyController', ['$scope', '$route', '$routeParams', '$location', function($scope, $route, $routeParams, $location) {
+  $scope.params = $routeParams;
+
+  $scope.apology = $scope.Apology.get({
+    apologyId: $scope.params.apologyId
   });
 }])
 
@@ -15,5 +27,9 @@ angular.module('app', ['ngResource', 'ngRoute', 'angularMoment'])
   $routeProvider
   .when('/', {
       templateUrl : "/templates/index.html"
+  })
+ .when('/apology/:user/:apologyId', {
+    templateUrl: '/templates/apology.html',
+    controller: 'ApologyController'
   });
 }]);

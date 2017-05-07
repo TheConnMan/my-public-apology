c4f7e3c77d20ae5f108d60f54086ce00ea2c172a
TheConnMan <bcconn2112@gmail.com>
2017-05-07 16:47:38 -0400

    Add additional logging

--- config/models.js
+++ config/models.js
@@ -1,5 +1,13 @@
+var sailsLogger = require('sails-persistence-logger')({
+  level: 'info'
+});
+
 module.exports.models = {
   connection: process.env.MYSQL_HOST ? 'mysql' : 'localDiskDb',
 
-  migrate: process.env.MYSQL_HOST ? 'safe' : 'alter'
+  migrate: process.env.MYSQL_HOST ? 'safe' : 'alter',
+
+  afterCreate: sailsLogger.afterCreate,
+  afterUpdate: sailsLogger.afterUpdate,
+  afterDestroy: sailsLogger.afterDestroy
 };

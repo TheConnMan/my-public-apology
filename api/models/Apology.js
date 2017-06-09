bee3ef8d13c232c6a22860a55a94e08a77f9ec20
TheConnMan <bcconn2112@gmail.com>
2017-06-08 21:36:53 -0400

    Tweet apologies after creation

--- api/models/Apology.js
+++ api/models/Apology.js
@@ -1,3 +1,8 @@
+var client = sails.config.globals.twitter.client;
+
+var log4js = require('log4js');
+var logger = log4js.getLogger('api/models/Apology');
+
 module.exports = {
 
   attributes: {
@@ -12,5 +17,26 @@ module.exports = {
     },
 
     user: { model: 'user' }
+  },
+
+  afterCreate: function(record, cb) {
+    if (client) {
+      (record.user ? User.findOne({ id: record.user}) : Promise.resolve()).then(user => {
+        var apologyLink = sails.config.serverUrl + '/#!/apology/' + (user ? user.name.split(' ').join('-') + '/' : '') + record.id;
+        var status = 'New apology from ' + (user ? user.name : '@' + record.tweetUser) + '! ' + apologyLink + (record.tweetId ? ' ' + getTweetUrl(record) : '');
+        client.post('statuses/update', {status: status}, function(error, tweet, response) {
+          if (error) {
+            logger.error(error);
+          }
+          cb();
+        });
+      });
+    } else {
+      cb();
+    }
   }
 };
+
+function getTweetUrl(record) {
+  return 'https://twitter.com/' + record.tweetUser + '/status/' + record.tweetId;
+}
